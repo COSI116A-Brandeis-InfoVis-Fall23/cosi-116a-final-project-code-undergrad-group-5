4cb06242026d3fa62d82f65e8cc6a0e914a4205c
Cindy Chi <cindychi@brandeis.edu>
2023-11-29 01:04:16 -0500

    added piechart

--- js/piechart.js
+++ js/piechart.js
@@ -1,26 +1,74 @@
-// Function to load and parse CSV file
-function loadCSVAndCreateChart() {
-    $.get('piechartinfo2csv.csv', function (csvData) {
-      // Parse CSV data
-      var rows = csvData.split('\n');
-      var data = [];
-      for (var i = 1; i < rows.length; i++) {
-        var cols = rows[i].split(',');
-        data.push({ x: cols[1], value: parseFloat(cols[2]) });
-      }
+
+
+
+// Load CSV data
+d3.csv("data/Cosi116aDataCleaning/ridership_by_line.csv", function (error, data) {
+  // Assuming the CSV has columns like 'service_date', 'route_or_line', 'average_monthly_ridership'
+
+  // Convert service_date to month format (assuming it's in the format 'YYYY/MM/DD')
+  data.forEach(entry => {
+    entry.month = entry.service_date.substring(0, 7);
+  });
+});
+
+
+
+// Function to generate a pie chart
+function generatePieChart(monthData) {
+  // Check if monthData is an object and is defined
+  console.log("the type of monthData is: ", typeof monthData);  // Output: object
+  console.log("monthData is: ", monthData)
   
-      // Create AnyChart pie chart
-      var customColors = ['#5733FF', '#33FF57', '#FFA500', '#FF5733'];
-      anychart.onDocumentReady(function () {
-        var chart = anychart.pie(data);
-        chart.palette(customColors);
-        chart.title('Ridership by MBTA Line Jan 2022');
-        chart.container('container');
-        chart.draw();
-      });
-    });
-  }
+    // Define a color scale based on the lines
+    const colorScale = d3.scaleOrdinal()
+    .domain(monthData.map(d => d.route_or_line))
+    .range(['blue', 'green', 'orange', 'red']);
+
+    // Generate pie chart
+    const pie = d3.pie();
+    const arc = d3.arc().innerRadius(0).outerRadius(150);
+
+    //Modify the sizings and spatial location of pie chart
+    const svg = d3.select("#piechartContainer").append("svg")
+    .attr("width", 500)
+    .attr("height", 500)
+    .append("g")
+    .attr("transform", "translate(200,200)");
+
+    // Each section of the pie chart is filled according to the MBTA line color they represent
+    svg.selectAll("path")
+    .data(pie(monthData.map(d => d.average_monthly_ridership)))
+    .enter().append("path")
+    .attr("d", arc)
+    .attr("fill", (d, i) => colorScale(monthData[i].route_or_line));
+
+    // Add labels for each slice
+    svg.selectAll("text")
+    .data(pie(monthData.map(d => d.average_monthly_ridership)))
+    .enter().append("text")
+    .attr("class", "pie-label") // Add a class for styling
+    .attr("transform", d => `translate(${arc.centroid(d)})`)
+    .attr("text-anchor", "middle")
+    .text((d, i) => monthData[i].route_or_line);
+
+    //Add title
+    svg.append("text")
+    .attr("x", 0)
+    .attr("y", -160) // Adjust the y-coordinate for the title position
+    .attr("text-anchor", "middle")
+    .style("font-size", "18px")
+    .style("font-weight", "bold")
+    .text("MBTA Ridership Breakdown By Line"); // Specify your title text
+
+ 
+
+    
+
+
+
   
-  // Call the function to load and create the chart
-  loadCSVAndCreateChart();
   
+  
+
+
+}
--- js/visualization.js
+++ js/visualization.js
@@ -1,31 +1,67 @@
-// Immediately Invoked Function Expression to limit access to our 
-// variables and prevent 
-((() => {
+(() => {
   d3.json("data/Cosi116aDataCleaning/avgMonthlyRidership.json", (data) => {
-    // General event type for selections, used by d3-dispatch
-    // https://github.com/d3/d3-dispatch
-    const dispatchString = "selectionUpdated";
+    d3.csv("data/Cosi116aDataCleaning/ridership_by_line.csv", (csvdata) => {
+      
+      const dispatchString = "selectionUpdated";
 
+      let tableData = table()
+        .selectionDispatcher(d3.dispatch(dispatchString))
+        ("#table", data);
 
-    // Create a table given the following: 
-    // a dispatcher (d3-dispatch) for selection events; 
-    // a div id selector to put our table in; and the data to use.
-    let bargraphData = bargraph()
-    .x(d => d.Month)
-    .xLabel("Month")
-    .y(d => d.Ridership)
-    .yLabel("Ridership")
-    .selectionDispatcher(d3.dispatch(dispatchString))
-    ("#bargraph", data);
+      // Function to convert word format month to numeric format
+      function convertToNumericMonth(wordMonth) {
+        const monthNames = [
+          "January", "February", "March", "April", "May", "June",
+          "July", "August", "September", "October", "November", "December"
+        ];
 
+        // Find the index of the selected month in the array
+        const index = monthNames.indexOf(wordMonth);
 
-    let tableData = table()
-      .selectionDispatcher(d3.dispatch(dispatchString))
-      ("#table", data);
+        // Convert the index to numeric format (adding 1 because array indices are zero-based)
+        const numericMonth = (index !== -1) ? (index + 1).toString().padStart(2, '0') : null;
 
+        return numericMonth;
+      }
 
-  })
+      // Function to handle row click
+      function handleRowClick(wordMonth) {
+        // Convert word format month to numeric format
+        const numericMonth = convertToNumericMonth(wordMonth);
+      
+        // Check if data is available for the selected month
+        if (numericMonth) {
+          // Filter data for the selected month. Should return 4 arrays of ridership by line for the certain month
+          const ridership_arrays = csvdata.filter(entry => {
 
+            // Extract month from the service_date column
+            const entryMonth = entry.service_date.split('/')[1];
+            return entryMonth === numericMonth;
+          });
+      
+          // Check if data is available for the selected month
+          if (ridership_arrays.length > 0) {
+            // Clear previous pie chart
+            d3.select("#piechartContainer").html("");
+            // Generate and display pie chart for each entry
+            console.log("ridership array is:", ridership_arrays); //make sure for each month selected there are 4 arrays for each line and ridership
+            generatePieChart(ridership_arrays)
 
-
-})());
+          } else {
+            console.log(`No data available for ${wordMonth}`);
+          }
+        } else {
+          console.log(`Invalid month format: ${wordMonth}`);
+        }
+      }
+      
+      // Attach click event listener to the table rows
+      document.getElementById("table").addEventListener("click", function (event) {
+        if (event.target.tagName === "TD") {
+          const selectedMonth = event.target.parentNode.cells[0].textContent; // Assuming the first cell contains the month
+          console.log("Selected Month:", selectedMonth);
+          handleRowClick(selectedMonth);
+        }
+      });
+  });
+})})();
