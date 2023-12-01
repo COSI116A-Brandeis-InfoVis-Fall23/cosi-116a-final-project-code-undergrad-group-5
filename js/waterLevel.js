318742b292594bf1d2ea26b00fa5ae9e9e446bb2
Cindy Chi <cindychi@brandeis.edu>
2023-11-30 21:35:18 -0500

    fixed duplicates

--- js/waterLevel.js
+++ js/waterLevel.js
@@ -1,64 +1,38 @@
-// Function to generate a water level visualization
 function generateWaterLevelVis(waterLevel) {
-    // Check if water level is an object and is defined
-    console.log("water level data is: ", waterLevel)
-  
-      // Generate waterLevel chart
-      //const outerRect = d3.path.rect();
-      //const innerRect = d3.path.rect();
+  // Check if water level is an object and is defined
+  console.log("water level data is: ", waterLevel);
 
-      var innerRect= d3.path(); 
-           
-         // Creating rectangle at x:50 and y:100 
-         // and height:200, width:300 
-         innerRect.rect(0,0,300,waterLevel);  
-         d3.select("#waterLevelContainer").attr("d",innerRect)
-         .attr("fill", (d, i) => 'blue');
-
-
-         
-  
-      //Modify the sizings and spatial location of water level container
-      const svg = d3.select("#waterLevelContainer").append("svg")
+  // Select the container and append an SVG
+  const svg = d3.select("#waterLevelContainer")
+      .append("svg")
       .attr("width", 500)
       .attr("height", 1000)
       .append("g")
       .attr("transform", "translate(0,0)");
-  
-      // the rectangle is filled based on the water level for the selected month
-      svg.selectAll("path")
-      .data(innerRect)
-      .enter().append("path")
-      .attr("d", innerRect)
-      .attr("fill", (d, i) => 'blue');
-  
-      // Add a label with the water level
-      svg.selectAll("text")
-      .data(innerRect)
-      .enter().append("text")
-      .attr("class", "water-label") // Add a class for styling
+
+  // Append a rectangle representing the water level
+  svg.append("rect")
+      .attr("width", 300)
+      .attr("height", waterLevel)
+      .attr("fill", "blue");
+
+  // Add a label with the water level
+  svg.append("text")
+      .attr("class", "water-label")
       .attr("text-anchor", "middle")
-      .text((d, i) => waterLevel);
-  
-      //Add title
-      svg.append("text")
-      .attr("x", 0)
-      .attr("y", -160) // Adjust the y-coordinate for the title position
+      .text(waterLevel)
+      .attr("x", 150) // Adjust the x-coordinate for label position
+      .attr("y", waterLevel + 20); // Adjust the y-coordinate for label position
+
+  // Add title
+  svg.append("text")
+      .attr("x", 250)
+      .attr("y", -160)
       .attr("text-anchor", "middle")
       .style("font-size", "18px")
       .style("font-weight", "bold")
-      .text("Monthly Water Level"); // Specify your title text
+      .text("Monthly Water Level");
+}
 
-    // function waterdata(d) {
-    //     return waterdata(d));
-    // }
-  }
-
-// var precipitation = 
-// var cap = 100
-//     percent=(percent/cap)
-// var width = percent*500
-
-// document.getElementById("myTank").style["border-bottom"]=width +"px solid green";
-// document.getElementById("myTank").style["height"]=500-width +"px";
-// document.getElementById("myTank").innerText = percent*100 +"%";
+// Example usage
+generateW
